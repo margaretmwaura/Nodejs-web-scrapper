e51cc5044eb62875b3d23f626367a2602d44178b
margaretmwaura <mwauramargaret1@gmail.com>
2022-10-04 17:12:52 +0300

    skeleton for the getting of the signed urks

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import { parse } from "node-html-parser";
 import fetch from "node-fetch";
+import axios from "axios";
 import fs from "fs";
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
-const require = createRequire(import.meta.url); // construct the require method
+
+// We have imported this to allow for importing of a json file
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
 import admin from "firebase-admin";
+import { isDataView } from "util/types";
 const serviceAccountKey = require("/Users/margaretmwaura/Downloads/web-scrapper-364504-firebase-adminsdk-ajo9y-3cac0a8d1e.json");
 
-// from "/Users/margaretmwaura/Downloads/web-scrapper-364504-firebase-adminsdk-ajo9y-3cac0a8d1e.json";
-
-//initialize the app
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
-  storageBucket: "web-scrapper-364504.appspot.com", //you can find in storage.
+  storageBucket: "web-scrapper-364504.appspot.com",
 });
 
-//get your bucket
 var bucket = admin.storage().bucket();
 
 //function to upload file
@@ -27,19 +27,63 @@ async function uploadFile(filepath, filename) {
       cacheControl: "public, max-age=31536000",
     },
   });
+}
 
-  console.log(`${filename} uploaded to bucket.`);
+// Store the signed url in the database
+async function generateSignedUrl(filename) {
+  const options = {
+    version: "v2",
+    action: "read",
+    expires: Date.now() + 1000 * 60 * 60,
+  };
+
+  const [url] = await bucket.file(filename).getSignedUrl(options);
+  return url;
+}
+
+async function downloadFile(srcFilename) {
+  const options = {
+    // The path to which the file should be downloaded, e.g. "./file.txt"
+    destination: `./audio/${srcFilename}`,
+  };
+
+  // Downloads the file
+  await bucket.file(srcFilename).download(options);
 }
 
 let root = [];
+let signedUrls = [];
+signedUrls.length = 52;
 
 const url = "https://www.rocketlanguages.com/french/lessons/french-alphabet";
 fetch(`${url}`)
   .then((res) => res.text())
   .then((body) => (root = parse(body)))
-  .then(() => extractData(root));
+  .then(async () => extractData(root));
 
-function extractData(root) {
+async function getAudio() {
+  let i = 0;
+  root.querySelectorAll("audio").forEach(async (audio_element) => {
+    const audio = audio_element.getAttribute("src");
+
+    const { data } = await axios.get(audio, {
+      responseType: "arraybuffer",
+      headers: {
+        "Content-Type": "audio/wav",
+      },
+    });
+    const outputFilename = `./${audio.split("/")[6]}`;
+    fs.writeFileSync(outputFilename, data);
+
+    // No space within names
+    await uploadFile(outputFilename, audio.split("/")[6]);
+    let url = await generateSignedUrl(audio.split("/")[6]);
+    signedUrls[i] = url;
+    i++;
+  });
+}
+
+async function extractData(root) {
   const description = root
     .querySelectorAll(".ws-french")[0]
     .querySelectorAll("div")[0]
@@ -51,11 +95,12 @@ function extractData(root) {
     .querySelectorAll("p")[0].text;
   console.log(description2);
 
-  const audio = root.querySelectorAll("audio")[0].getAttribute("src");
-  let filepath =
-    "/Users/margaretmwaura/Downloads/web-scrapper-364504-firebase-adminsdk-ajo9y-3cac0a8d1e.json";
-  let filename = "test_file"; //can be anything, it will be the name with which it will be uploded to the firebase storage.
+  await getAudio();
+  console.log(signedUrls);
 
-  uploadFile(filepath, filename);
-  console.log(audio);
+  // FIXME: No need for this really
+  // await downloadFile(audio.split("/")[6]);
+
+  // console.log(data);
+  // console.log(audio.split("/")[6]);
 }
