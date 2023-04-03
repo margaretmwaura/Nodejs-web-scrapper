87aa91b05a08d1925963589d4867d17dbc7a239f
margaretmwaura <mwauramargaret1@gmail.com>
2023-04-03 11:44:28 +0300

    saving file name as a string

--- db/dbdump.sql
+++ db/dbdump.sql
@@ -1,7 +1,7 @@
 CREATE TABLE vowels ( id int(11) unsigned NOT NULL AUTO_INCREMENT, 
 name varchar(200) DEFAULT NULL, 
 description varchar(200) DEFAULT NULL, 
-filename BLOB DEFAULT NULL, 
+filename varchar(2000) DEFAULT NULL, 
 createdAt timestamp NULL DEFAULT NULL, 
 deletedAt timestamp NULL DEFAULT NULL, 
 updatedAt timestamp NULL DEFAULT NULL, 
