062be420839d0fcbe0773035b40e6ccce47abb8b
margaretmwaura <mwauramargaret1@gmail.com>
2023-07-11 05:52:09 +0300

    using the cron library for cron jobs

--- web/crons/index.js
+++ web/crons/index.js
@@ -1,8 +1,29 @@
-const schedule = require("node-schedule");
+// const schedule = require("node-schedule");
 
-const { getAllData } = require("./jobs/scrapVowels");
-schedule.scheduleJob("20 6 * * *", () => {
-  getAllData();
-});
+// const { getAllData } = require("./jobs/scrapVowels");
+// schedule.scheduleJob("20 6 * * *", () => {
+//   getAllData();
+// });
 
-// 0 5 * * *
+// // 0 5 * * *
+
+async function testingCrons() {
+  const CronJob = require("./../lib/cron.js").CronJob;
+
+  console.log("Before job instantiation");
+  let date = new Date();
+  date.setSeconds(date.getSeconds() + 2);
+  const job = new CronJob(date, function () {
+    const d = new Date();
+    console.log("Specific date:", date, ", onTick at:", d);
+  });
+  console.log("After job instantiation");
+  job.start();
+}
+
+testingCrons();
+
+// Managers
+// https://github.com/cfurst/CronJobManager
+// https://medium.com/m2p-yap-fintech/building-a-cron-job-manager-from-scratch-using-node-js-d7abc7a3646b
+// https://m2pfintech.com/blog/building-a-cron-job-manager-from-scratch-using-node-js/
---
+++ web/crons/jobs/reminder.js
@@ -0,0 +1,3 @@
+// https://stackoverflow.com/questions/42621024/run-a-cron-job-that-depends-on-entries-of-a-database-in-nodejs-using-aws
+// https://github.com/lykmapipo/kue-scheduler
+// https://stackoverflow.com/questions/33993130/dynamically-update-nodejs-cronjob-period#:~:text=In%20case%20you%20want%20to,the%20functions%20a.
